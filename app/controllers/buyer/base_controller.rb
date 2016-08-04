9f8446a0648f235b50f6b83f7e06f0deb97eca7c
narendra <narendra.prajapat@brsoftech.org>
2016-08-04 14:28:32 +0530

    uploading buyer dashboard

--- app/controllers/buyer/base_controller.rb
+++ app/controllers/buyer/base_controller.rb
@@ -1,4 +1,12 @@
 class Buyer::BaseController < ApplicationController
 	layout 'buyer'
 	before_filter :authenticate_user!
+	before_filter :check_user
+	
+	def check_user
+		if current_user.role == 1
+			redirect_to root_path, notice: "You must be a buyer to access this area !"
+		end
+	end
+	
 end
--- app/controllers/seller/base_controller.rb
+++ app/controllers/seller/base_controller.rb
@@ -1,4 +1,12 @@
 class Seller::BaseController < ApplicationController
 	layout 'seller'
 	before_filter :authenticate_user!
+	before_filter :check_user
+	
+	def check_user
+		if current_user.role == 2
+			redirect_to root_path, notice: "You must be a seller to access this area !"
+		end
+	end
+		
 end
