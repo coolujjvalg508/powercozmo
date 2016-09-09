d9488fde44d6dbbac1c22412552c6b24664b6763
narendra <narendra.prajapat@brsoftech.org>
2016-09-09 12:15:38 +0530

    test

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -167,10 +167,10 @@ class WelcomeController < ApplicationController
 					#abort(sub_search_query)
 				end
 				
-				#abort(search_query)
+				abort(search_query)
 				#abort(search_arr.to_json)
 				
-				equipment_result = Equipment.available_for_purchase.joins(:country, :manufacturer, :category, :user).where('equipment.user_id != ? AND equipment.created_at > ' + last_search_time + ' AND ' + search_query, user_id)
+				equipment_result = Equipment.available_for_purchase.joins(:country, :manufacturer, :category, :user).where('equipment.user_id != ? AND equipment.created_at > ? AND ' + search_query, user_id, last_search_time)
 				abort(equipment_result.to_json)
 				if !equipment_result.empty?
 				
